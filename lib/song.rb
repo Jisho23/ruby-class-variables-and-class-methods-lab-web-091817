7e68aabc62fb5e6672339848d78f6ef20b2ce755
github username <github email address>
2017-09-21 13:59:08 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require "pry"
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    count_hash = {}
+    @@artists.each do |artist|
+      unless count_hash[artist]
+        count_hash[artist] = 1
+      else
+        count_hash[artist] += 1
+      end
+    end
+    count_hash
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genre_count
+    count_hash = {}
+    @@genres.each do |genre|
+      unless count_hash[genre]
+        count_hash[genre] = 1
+      else
+        count_hash[genre] += 1
+      end
+    end
+    count_hash
+  end
+end
